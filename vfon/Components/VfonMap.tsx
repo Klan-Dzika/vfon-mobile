ff8fe458db5f679be2f654a84ecfa06479c51ed0
Filip Mazur <flp.mazur@gmail.com>
2021-05-30 15:18:44 +0200

    Integration with Azure Function to display markers on a map

--- vfon/Components/VfonMap.tsx
+++ vfon/Components/VfonMap.tsx
@@ -4,8 +4,20 @@ import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import GetLocation, {Location} from 'react-native-get-location';
+import { getUniqueId, getDeviceName } from 'react-native-device-info';
+
+interface Dzik {
+  ID: string;
+  name: string;
+  lat: number;
+  lng: number;
+  direction: number;
+  speed: number;
+  status: string;
+  timestamp: string;
+}
 
 const VfonMap: React.FC = () => {
   const getLocation = async (): Promise<Location | undefined> => {
@@ -23,25 +35,65 @@ const VfonMap: React.FC = () => {
     }
   };
 
+  const [dziki, setDziki] = useState<Dzik[]>();
+  const deviceId = getUniqueId();
+  let location: Location | undefined;
+
+  const refresh = async () => {
+    try {
+      location = await getLocation();
+      const dzik: Dzik = {
+        ID: deviceId,
+        direction: 0,
+        lat: location?.latitude ?? 0,
+        lng: location?.longitude ?? 0,
+        name: await getDeviceName(),
+        speed: 0,
+        status: 'OK',
+        timestamp: new Date().toISOString(),
+      };
+
+      let response = await fetch(
+        'https://dzikboytest.azurewebsites.net/api/sync',
+        {body: JSON.stringify(dzik), method: 'POST'},
+      );
+      let json = await response.json();
+      setDziki(json);
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // setInterval(refresh, 4000);
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
   return (
     <MapView
       customMapStyle={mapStyle}
       provider={PROVIDER_GOOGLE}
       style={{width: wp(100), height: hp(100)}}
       initialRegion={{
-        latitude: 51.117,
-        longitude: 0.192,
+        latitude: location?.latitude ?? 0,
+        longitude: location?.longitude ?? 0,
         latitudeDelta: 0.011,
         longitudeDelta: 0.011,
       }}>
-      <Marker
-        // image={require('../assets/cap_icon.png')} // this scales better on Android
-        // image={{uri: 'custom_pin'}}
-        coordinate={{
-          latitude: 51.117,
-          longitude: 0.192,
-        }}
-      />
+      {dziki?.map(dzik => (
+        <Marker
+          // image={require('../assets/cap_icon.png')} // this scales better on Android
+          // image={{uri: 'custom_pin'}}
+          key={dzik.ID}
+          title={dzik.name}
+          coordinate={{
+            latitude: dzik.lat,
+            longitude: dzik.lng,
+          }}
+        />
+      ))}
     </MapView>
   );
 };
